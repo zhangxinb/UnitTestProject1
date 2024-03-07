45bf5ec0611e0ec4773efb24f0448bbab33b9909
ZhangXin <zhang-xin@hotmail.com>
2024-03-07 21:15:10 +0200

    Updated project ID for "BankToBeTested"
    Updated the project reference ID for "BankToBeTested" in the "UnitTestProject1.csproj" file and the project ID in the "UnitTestProject1.sln" file. Also, the project configuration platforms for "BankToBeTested" in the "UnitTestProject1.sln" file have been updated to reflect the new project ID, and the configurations for the old project ID have been removed.

--- BankToBeTested/Program.cs
+++ BankToBeTested/Program.cs
@@ -4,21 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BankToBeTested
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace UnitTestProject1
 {
     public class Bank
